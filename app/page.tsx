8e8d3c96297ed22e4a58094f362d5d46e67048a0
femi abimbola <femiabimbola6@gmail.com>
2025-06-12 12:39:34 -0700

    created the login and the register page

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,10 @@ const  Home = () => {
     <div className="flex flex-col justify-center items-center h-screen gap-y-3">
       <h1 className="text-3xl font-bold text-white"> A 2 Factor Authentication</h1>
       <p className="text-white pb-3">A Two Factor Authentication with Speakeasy and Jsonwebtoken</p>
-      
-        <Link href={'/login'} className="px-8 py-3 rounded-sm bg-blue-600 text-white cursor-pointer"> Login  </Link>      
+      <div className="flex gap-x-8">
+      <Link href={'/login'} className="px-8 py-2.5 rounded-sm bg-blue-600 text-white cursor-pointer"> Login  </Link>
+      <Link href={'/register'} className="px-8 py-2.5 rounded-sm bg-blue-600 text-white cursor-pointer"> Register  </Link>
+      </div>
       
     </div>
     
