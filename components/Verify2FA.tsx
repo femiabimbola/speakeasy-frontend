b9574f1b735d8392031ff58dc0350dc4cf686ba7
femi abimbola <femiabimbola6@gmail.com>
2025-06-27 23:53:30 -0700

    working on the verify2fa

--- components/Verify2FA.tsx
+++ components/Verify2FA.tsx
@@ -5,6 +5,14 @@ import { Form,FormControl, FormField,
   FormItem, FormLabel, FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp"
+
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -22,25 +30,35 @@ const verifySchema = z.object({
 });
 
 export const Verify = () => {
-  const dispatch = useAppDispatch();
 
-  const fetchUser = async () => {
-    try {
-      const result = await dispatch(getUser()).unwrap();
-    } catch (error) {
-      console.error('Error fetching user:', error);
-    }
-  };
+  const [otp, setOtp] = useState('')
 
   useEffect(() => {
-    fetchUser()
-  }, [])
+    if (otp.length === 6) {
+      handleVerify()
+    }
+  }, [otp])
+
+  
+  // const dispatch = useAppDispatch();
+
+  // const fetchUser = async () => {
+  //   try {
+  //     const result = await dispatch(getUser()).unwrap();
+  //   } catch (error) {
+  //     console.error('Error fetching user:', error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchUser()
+  // }, [])
   
 
-  const form = useForm<z.infer<typeof verifySchema>>({
-    resolver: zodResolver(verifySchema),
-    defaultValues: { input: "" },
-  });
+  // const form = useForm<z.infer<typeof verifySchema>>({
+  //   resolver: zodResolver(verifySchema),
+  //   defaultValues: { input: "" },
+  // });
 
 const onSubmit = (values: z.infer<typeof verifySchema>) => {
   // handleTokenVerification(values)
@@ -53,38 +71,19 @@ const router = useRouter();
   return (
     <div className="h-screen flex items-center justify-center">
     <CardWrapper>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <div className=" flex flex-col gap-y-5">
-            <h1 className="text-2xl font-bold tracking-tight text-gray-900 text-center"> Verify TOTP</h1>
-            <FormField
-                control={form.control}
-                name="input"
-                render={({field}) => (
-                  <FormItem>
-                    <FormLabel>TOTP</FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        placeholder="Enter your TOTP"
-                        type="number"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-             <FormError message={error} />
-              <FormSuccess message={success} />
-             <Button type="submit" className="cursor-pointer">Submit</Button>
-             <div>
-             </div>
-          </div>
-        </form>
-      </Form>
-      {/* <Button onClick={handleResetTOTP} className="cursor-pointer"> 
-        Reset TOTP
-      </Button> */}
+    <InputOTP maxLength={6}>
+      <InputOTPGroup>
+        <InputOTPSlot index={0} />
+        <InputOTPSlot index={1} />
+        <InputOTPSlot index={2} />
+      </InputOTPGroup>
+      <InputOTPSeparator />
+      <InputOTPGroup>
+        <InputOTPSlot index={3} />
+        <InputOTPSlot index={4} />
+        <InputOTPSlot index={5} />
+      </InputOTPGroup>
+    </InputOTP>
     </CardWrapper>
   </div>
   )
