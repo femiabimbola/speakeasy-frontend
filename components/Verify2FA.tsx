09744e18dd88ed1290795c969b748e0720e97220
femi abimbola <femiabimbola6@gmail.com>
2025-06-28 01:10:00 -0700

    working on verify2fa

--- components/Verify2FA.tsx
+++ components/Verify2FA.tsx
@@ -1,8 +1,13 @@
-"use client"
+"use client";
 
 import { getUser } from "@/redux/actions/userAction";
-import { Form,FormControl, FormField,
-  FormItem, FormLabel, FormMessage,
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
@@ -11,7 +16,7 @@ import {
   InputOTPGroup,
   InputOTPSeparator,
   InputOTPSlot,
-} from "@/components/ui/input-otp"
+} from "@/components/ui/input-otp";
 
 import { z } from "zod";
 import { useForm } from "react-hook-form";
@@ -24,68 +29,66 @@ import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
 import { FormError, FormSuccess } from "./FormMessage";
 
-
 const verifySchema = z.object({
   input: z.string().min(1, { message: "Password is required" }),
 });
 
 export const Verify = () => {
-
-  const [otp, setOtp] = useState('')
+  const [otp, setOtp] = useState("");
 
   useEffect(() => {
     if (otp.length === 6) {
-      handleVerify()
+      handleVerify();
     }
-  }, [otp])
+  }, [otp]);
 
-  
-  // const dispatch = useAppDispatch();
+  const handleVerify = async () => {
+    setError("")
+    try {
+      
+    } catch (error) {
+      
+    }
+  };
 
-  // const fetchUser = async () => {
-  //   try {
-  //     const result = await dispatch(getUser()).unwrap();
-  //   } catch (error) {
-  //     console.error('Error fetching user:', error);
-  //   }
-  // };
+  const onSubmit = (values: z.infer<typeof verifySchema>) => {
 
-  // useEffect(() => {
-  //   fetchUser()
-  // }, [])
-  
+  };
 
-  // const form = useForm<z.infer<typeof verifySchema>>({
-  //   resolver: zodResolver(verifySchema),
-  //   defaultValues: { input: "" },
-  // });
-
-const onSubmit = (values: z.infer<typeof verifySchema>) => {
-  // handleTokenVerification(values)
-}
-
-const [error, setError] = useState<string | undefined>("");
-const [success, setSuccess] = useState<string | undefined>("");
-const router = useRouter();
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const router = useRouter();
 
   return (
     <div className="h-screen flex items-center justify-center">
-    <CardWrapper>
-    <InputOTP maxLength={6}>
-      <InputOTPGroup>
-        <InputOTPSlot index={0} />
-        <InputOTPSlot index={1} />
-        <InputOTPSlot index={2} />
-      </InputOTPGroup>
-      <InputOTPSeparator />
-      <InputOTPGroup>
-        <InputOTPSlot index={3} />
-        <InputOTPSlot index={4} />
-        <InputOTPSlot index={5} />
-      </InputOTPGroup>
-    </InputOTP>
-    </CardWrapper>
-  </div>
-  )
-
+      <CardWrapper>
+        <div className=" flex flex-col gap-y-8">
+          <h1 className="text-xl font-bold tracking-tight text-gray-900 text-center">
+            Activate Two Factor Authentication
+          </h1>
+          <FormError message={error} />
+          <div className="flex justify-center">
+            <InputOTP
+              maxLength={6}
+              value={otp}
+              onChange={(value) => setOtp(value)}
+              onComplete={handleVerify}
+            >
+              <InputOTPGroup>
+                <InputOTPSlot className=" border-gray-700" index={0} />
+                <InputOTPSlot className="border-gray-700" index={1} />
+                <InputOTPSlot className=" border-gray-700" index={2} />
+              </InputOTPGroup>
+              <InputOTPSeparator />
+              <InputOTPGroup>
+                <InputOTPSlot className=" border-gray-700" index={3} />
+                <InputOTPSlot className=" border-gray-700" index={4} />
+                <InputOTPSlot className=" border-gray-700" index={5} />
+              </InputOTPGroup>
+            </InputOTP>
+          </div>
+        </div>
+      </CardWrapper>
+    </div>
+  );
 };
